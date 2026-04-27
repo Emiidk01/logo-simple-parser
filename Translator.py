d260dba834e7c2ce3a719ddb764f290fdfe87f7b
Emiliano <a01710711@tec.mx>
2026-04-27 13:58:36 -0600

    Add the rest of the node

--- Translator.py
+++ Translator.py
@@ -45,6 +45,117 @@ class Minus(Numeric):
 
     def eval(self, env):
         return -1 * float(self.right.eval(env))
+    
+class Not(Logic):
+    def __init__(self, right):
+        self.right = right
+
+    def eval(self, env):
+        return not bool(self.right.eval(env))
+
+
+class Less(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left < right
+
+class LessEq(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left <= right
+
+class Greater(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left > right
+
+class GreaterEq(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        left = float(self.left.eval(env))
+        right = float(self.right.eval(env))
+        return left >= right
+    
+class Equal(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        return self.left.eval(env) == self.right.eval(env)
+
+class NotEqual(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        return self.left.eval(env) != self.right.eval(env)
+    
+class And(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        return bool(self.left.eval(env)) and bool(self.right.eval(env))
+
+class Or(Logic):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def eval(self, env):
+        return bool(self.left.eval(env)) or bool(self.right.eval(env))
+
+class Print(Void):
+    def __init__(self, value):
+        self.value = value
+
+    def eval(self, env):
+        print(self.value.eval(env))
+
+class Assignment(Void):
+    def __init__(self, name, value, line):
+        self.name = name
+        self.value = value
+        self.line = line
+
+    def eval(self, env):
+        value = self.value.eval(env)
+        result = env.set(self.name, None, value)
+        if not result:
+            text = "Line " + str(self.line) + " - " + self.name + " has not been declared"
+            raise Exception(text)
+        
+class Declaration(Void):
+    def __init__(self, name, line):
+        self.name = name
+        self.line = line
+    
+    def eval(self, env):
+        result = env.insert(self.name)
+        if not result:
+            text = "Line " + str(self.line) + " - " + self.name + " has already been declared"
+            raise Exception(text)
 
 class Add(Numeric):
     def __init__(self, left, right):
